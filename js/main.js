5b2f67b44409b30db5686e59ad178f87cacdef39
blaaaaaaam <alex.tanazefti@gmail.com>
2021-03-30 19:49:03 +0200

    update with all content and mobile version

---
+++ js/main.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+	// Change Background
+	let options = {
+		rootMargin: '-24.99% 0px -75% 0px',
+		threshold: 0
+	}
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id')
+			const gradient = entry.target.dataset.gradient
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).classList.add('active')
+				document.getElementsByClassName(gradient)[0].style.opacity = 1
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).classList.remove('active')
+				document.getElementsByClassName(gradient)[0].style.opacity = 0
+			}
+		});
+	},options);
+
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+
+	// Initialise Plyrs
+	const players = Array.from(document.querySelectorAll('.player')).map(
+		p => new Plyr(p, {
+			controls:['play'],
+			keyboard:{focused:false,global:false},
+			fullscreen: {enabled: false},
+			clickToPlay: false,
+			autopause: false,
+			hideControls: false,
+		})
+	)
+
+	// Initialise carousels
+	const carousels = Array.from(document.querySelectorAll('.carousel')).map(
+		c => new Flickity(c, {
+			imagesLoaded: true
+		})
+	)
+
+	// Fire a resize event to get the height of videos
+	const videos = Array.from(document.querySelectorAll('video')).map(
+		v => v.addEventListener('loadeddata', (event) => {
+			let loaded = document.createEvent('HTMLEvents')
+			loaded.initEvent('resize', true, false);
+			document.dispatchEvent(loaded);
+		})
+	)
+});
